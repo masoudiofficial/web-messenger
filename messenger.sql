64c0f2a77a237a97bf142326f6a52168ad836c85
Hamid Masoudi <87407109+masoudiofficial@users.noreply.github.com>
2025-10-16 13:04:57 +0330

    Add files

---
+++ messenger.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 16, 2025 at 11:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `messenger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userstable`
+--
+
+CREATE TABLE `userstable` (
+  `username` char(32) NOT NULL,
+  `password` char(255) NOT NULL,
+  `creationdate` datetime NOT NULL,
+  `messages` mediumtext NOT NULL,
+  `status` char(19) NOT NULL,
+  `blocks` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userstable`
+--
+ALTER TABLE `userstable`
+  ADD UNIQUE KEY `username` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
