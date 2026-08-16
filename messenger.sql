d1d43487dc1c0bf813d907101d29f5e4dcbba29f
Hamid Masoudi <87407109+masoudiofficial@users.noreply.github.com>
2026-08-16 12:31:38 +0330

    Update messenger.sql

--- messenger.sql
+++ messenger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2026 at 01:14 PM
+-- Generation Time: Aug 16, 2026 at 10:56 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
